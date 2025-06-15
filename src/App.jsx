de532e980e57f920241871c78c33aa740c9eea2f
thinkand-port <your@email.com>
2025-06-15 21:07:59 +0530

    changed something

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import { ToastContainer } from "react-toastify";
+
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
